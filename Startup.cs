1acf1e1e42970945ad32b1921eb998188826ab03
Igor Ziselman <igor.zc@gmail.com>
2020-11-04 21:47:34 +0200

    fixed stattup issue

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,20 @@ namespace Microsoft.BotBuilderSamples
         {
             services.AddControllers().AddNewtonsoftJson();
 
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+            
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();            
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // // The Dialog that will be run by the bot.
+            // services.AddSingleton<UserProfileDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, EchoBot>();
